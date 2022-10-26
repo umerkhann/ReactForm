2e8834191f427b83cb859d70649fb4fa20aea685
umerkhann <umeriskhan7@gmail.com>
2022-10-26 20:49:33 +0500

    Form app Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Form">
+      <h1>Contact Form</h1>
+      <form className='form-container'>
+        <input type="text" placeholder='Name'/> 
+        <input type="text" placeholder='Email'/> 
+        <input type="text" placeholder='Phone'/> 
+        <input type="text" placeholder='Organization'/> 
+        <textarea rows='8' cols='8' placeholder='Message'/>
+        <button className='submit-btn'>Submit</button>
+      </form>
     </div>
   );
 }
